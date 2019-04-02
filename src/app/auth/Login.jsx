4c631380c9f60f3d9e732abfc56de9a6a432dc40
yizhang199 <yizhang19801001@gmail.com>
2019-04-02 13:33:29 +1100

    not important stuffs

---
+++ src/app/auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Form from "./Form";
+const Login = () => {
+  return (
+    <div className="component-login">
+      <Form />
+    </div>
+  );
+};
+
+export default Login;
