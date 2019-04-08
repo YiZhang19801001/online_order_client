d28520da5564ae548ab4ca5c5c8456f48e8d72de
yizhang199 <yizhang19801001@gmail.com>
2019-04-08 10:58:57 +1000

    1. send request with api_token

--- src/_helpers/api.js
+++ src/_helpers/api.js
@@ -1,13 +1,25 @@
 import axios from "axios";
 
+//* params
+const baseURL = [
+  `http://localhost:8000/api`,
+  `http://kidsnparty.com.au/roben_api/groupon/public/api`,
+  `http://localhost/groupon_api/public/api`
+];
+
+//* funtions
 const makeRequest = () => {
+  const localStorageUser = localStorage.getItem("staff_order_user");
+  const user = localStorageUser ? JSON.parse(localStorageUser) : null;
+  const api_token = user ? user.api_token : null;
+  const headers = api_token ? { Authorization: `Bearer ${api_token}` } : {};
   return axios.create({
-    baseURL: `http://localhost:8000/api`
-    // baseURL: `http://kidsnparty.com.au/roben_api/groupon/public/api`
-    // baseURL: `http://localhost/groupon_api/public/api`
+    baseURL: baseURL[0],
+    headers
   });
 };
 
-const request = makeRequest();
+// * exports stuffs
+const api = makeRequest();
 
-export default request;
+export { api };
