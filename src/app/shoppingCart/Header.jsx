5a70c35dfb0ac8b114d22d2aa648a0aead7222dd
yizhang199 <yizhang19801001@gmail.com>
2019-04-08 16:43:19 +1000

    improve: not trigger event when list is empty

--- src/app/shoppingCart/Header.jsx
+++ src/app/shoppingCart/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default ({ title, className, toggle, quantity }) => {
   return (
-    <div className={className} onClick={toggle}>
+    <div className={className} onClick={quantity > 0 ? toggle : () => {}}>
       <span>{title}</span>
       <span>{quantity}</span>
     </div>
